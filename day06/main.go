10206f102bd831c4c2d60928944a1f39b3f36240
StefanS <stefan.samuelsson@packsize.com>
2021-12-06 16:06:43 +0100

    Day06-2

--- day06/main.go
+++ day06/main.go
@@ -34,19 +34,29 @@ func part1() int {
 	return len(val)
 }
 func part2() int {
+	// part1 is way too slow and uses so much memory that I had to restart it... So this solution uses groups instead
 	inp := c.ReadInputFile()
 	val := c.GetInts(strings.Split(inp[0], ","))
+	res := map[int]int{0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
-	for day := 0; day < 80; day++ {
-
-		for i := range val {
-			if val[i] == 0 {
-				val = append(val, 8)
-				val[i] = 7
-			}
-			val[i]--
-		}
+	for _, v := range val {
+		res[v]++
 	}
 
-	return len(val)
+	for day := 0; day < 256; day++ {
+		for p := 0; p <= 8; p++ {
+			if p == 0 {
+				res[7] += res[0]
+				res[9] = res[0]
+			}
+			res[p] = res[p+1]
+
+		}
+	}
+	sum := 0
+	for i := 0; i < 9; i++ {
+		sum += res[i]
+	}
+
+	return sum
 }
